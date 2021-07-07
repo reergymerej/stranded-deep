2af8fe4ad7791d48d92ab19623e02163668a7efb
Jeremy Greer <jex.grizzle@gmail.com>
2021-07-07 19:03:16 -0400

    Look at degrees when comparing islands

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,14 +1,57 @@
 import * as mod from '.'
 
-fdescribe('islands', () => {
+const getMeasurement = (data: Partial<mod.Measurement> = {
+}): mod.Measurement => ({
+  degrees: 0,
+  distanceEstimate: 0,
+  ...data,
+})
+
+describe('islands', () => {
   describe('comparing', () => {
     describe('when they are quite different', () => {
       it('should give a low match likelihood', () => {
-        const islandA = 1
-        const islandB = 1
-        const compare = () => 0.1
+        const islandA: mod.Island = [
+          getMeasurement({
+            degrees: 0,
+          }),
+        ]
+        const islandB: mod.Island = [
+          getMeasurement({
+            degrees: 1,
+          }),
+        ]
         expect(mod.compare(islandA, islandB)).toBeLessThan(0.5)
       })
+    })
+
+    describe('when they are quite similar', () => {
+      it('should give a high match likelihood', () => {
+        const islandA: mod.Island = [
+          getMeasurement({
+            degrees: 1,
+          }),
+        ]
+        const islandB: mod.Island = [
+          getMeasurement({
+            degrees: 0.8,
+          }),
+        ]
+        expect(mod.compare(islandA, islandB)).toBeGreaterThan(0.5)
+      })
+    })
+  })
+
+  describe('getValue', () => {
+    it('should quantify an island', () => {
+      const islandA: mod.Island = [
+        getMeasurement({
+          degrees: 1,
+        }),
+      ]
+      const actual = mod.getValue(islandA)
+      const expected = 1
+      expect(actual).toBe(expected)
     })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
-type Island = number
-export const compare = (a: Island, b: Island): number => 0.1
+enum Estimate {
+  CLOSE,
+  MEDIUM,
+  FAR,
+}
+
+type Time = {
+  hours: number,
+  minutes: number,
+}
+
+export type Measurement = {
+  degrees: number,
+  distanceEstimate: Estimate,
+  distanceMeasurement?: Time,
+  origin?: boolean,
+  next?: boolean,
+}
+
+export type Island = Measurement[]
+
+export const getValue = (island: Island): number => {
+  return island[0].degrees
+}
+
+export const compare = (a: Island, b: Island): number => {
+  const aValue = getValue(a)
+  const bValue = getValue(b)
+  const similarity = 1 - (Math.abs(aValue - bValue))
+  return similarity
+}
