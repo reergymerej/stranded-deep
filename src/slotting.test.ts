239204d733afd4cd80698de88c921274b266682f
Jeremy Greer <jex.grizzle@gmail.com>
2021-07-08 19:07:49 -0400

    Get left side of slots

--- src/slotting.test.ts
+++ src/slotting.test.ts
@@ -1,8 +1,16 @@
 import { getDegreeDiff } from '.'
 
-const getRight = (a: number, b: number): number => {
-  const diff = getDegreeDiff(a, b)
+const getRight = (a: number, afterA: number): number => {
+  const diff = getDegreeDiff(a, afterA)
   return (a + diff/2) % 360
+}
+
+const getLeft = (a: number, afterA: number): number => {
+  const diff = getDegreeDiff(a, afterA)
+  const r = (a - diff/2)
+  return r < 0
+    ? 360 + r
+    : r
 }
 
 describe('getting right', () => {
@@ -27,8 +35,36 @@ describe('getting right', () => {
       60,
       92,
     ],
-  ])('%s = %s, %s', (expected, a, b) => {
-    const actual = getRight(a, b)
+  ])('%s = %s, %s', (expected, a, afterA) => {
+    const actual = getRight(a, afterA)
+    expect(actual).toBe(expected)
+  })
+})
+
+describe('getting left', () => {
+  it.each([
+    [
+      45,
+      90,
+      0,
+    ],
+    [
+      135,
+      180,
+      90,
+    ],
+    [
+      225,
+      270,
+      180,
+    ],
+    [
+      315,
+      0,
+      270,
+    ],
+  ])('%s = %s, %s', (expected, a, beforeA) => {
+    const actual = getLeft(a, beforeA)
     expect(actual).toBe(expected)
   })
 })
