77d5cf81a9c4a6190384eac59e04908973e70aa4
Jeremy Greer <jex.grizzle@gmail.com>
2021-07-11 13:08:34 -0400

    Change types to distinguish between named island and fingerprint

--- src/stitcher/index.test.ts
+++ src/stitcher/index.test.ts
@@ -1,26 +1,164 @@
 import { Measurement, Estimate } from '../'
+import * as main from '../'
+
 const ftest = fit
 
-type Island = Measurement[]
+type Fingerprint = Measurement[]
+
+type NamedIsland = {
+  fingerprint: Fingerprint,
+  name: string,
+}
 
 type LogEntry = {
-  origin: Island | null,
-  measurements: Measurement[],
+  origin: Fingerprint | null,
+  fingerprint: Fingerprint,
   next: Measurement | null,
 }
 type Log = LogEntry[]
 
 type RouteEntry = {
-  location: Island | null,
-  origin: Island | null,
+  origin: Fingerprint | null,
+  location: Fingerprint,
+  locationName?: string,
 }
 
 type Route = RouteEntry[]
 
+const fingerprints: {[key: string]: Fingerprint} = {
+  A: [
+    {
+      degrees: 90,
+      distanceEstimate: Estimate.MEDIUM,
+    },
+    {
+      degrees: 180,
+      distanceEstimate: Estimate.MEDIUM,
+    },
+  ],
+  B: [
+    {
+      degrees: 225,
+      distanceEstimate: Estimate.MEDIUM,
+    },
+    {
+      degrees: 270,
+      distanceEstimate: Estimate.MEDIUM,
+    },
+  ],
+  C: [
+    {
+      degrees: 45,
+      distanceEstimate: Estimate.MEDIUM,
+    },
+    {
+      degrees: 0,
+      distanceEstimate: Estimate.MEDIUM,
+    },
+  ],
+  Boop: [
+    {
+      distanceEstimate: Estimate.FAR,
+      degrees: 99,
+    },
+  ],
+  BoopOpposite: [
+    {
+      distanceEstimate: Estimate.CLOSE,
+      degrees: 279,
+    },
+  ],
+  Beep: [
+    {
+      distanceEstimate: Estimate.FAR,
+      degrees: 95,
+    },
+  ],
+  NotInList: [
+    {
+      distanceEstimate: Estimate.FAR,
+      degrees: 11,
+    },
+  ],
+}
+
+describe('identifying a NamedIsland by fingerprint', () => {
+  const list: NamedIsland[] = [
+    {
+      fingerprint: fingerprints.A,
+      name: 'Alpha',
+    },
+    {
+      fingerprint: fingerprints.B,
+      name: 'Bravo',
+    },
+    {
+      fingerprint: fingerprints.C,
+      name: 'Charlie',
+    },
+    {
+      fingerprint: fingerprints.Boop,
+      name: 'Belle',
+    },
+  ]
+
+  const findIslandInList = (
+    list: NamedIsland[],
+    fingerprint: Fingerprint,
+    threshold = 0.7,
+  ): NamedIsland | undefined =>
+    list.find(
+      namedIsland =>
+      main.compareIslands(fingerprint, namedIsland.fingerprint) >= threshold
+    )
+
+  describe('when there is no match', () => {
+    it('should return undefined', () => {
+      const fingerprint: Fingerprint = fingerprints.NotInList
+      const actual = findIslandInList(list, fingerprint)
+      const expected = undefined
+      expect(actual).toEqual(expected)
+    })
+  })
+
+  describe('when there is no close match', () => {
+    it('should return undefined', () => {
+      const fingerprint: Fingerprint = fingerprints.BoopOpposite
+      const actual = findIslandInList(list, fingerprint)
+      const expected = undefined
+      expect(actual).toEqual(expected)
+    })
+  })
+
+  describe('when there is a close match', () => {
+    it('should return the matched NamedIsland', () => {
+      const fingerprint: Fingerprint = fingerprints.Beep
+      const actual = findIslandInList(list, fingerprint)
+      const expected: NamedIsland = list.find(x => x.name === 'Belle')
+      expect(actual).toEqual(expected)
+    })
+  })
+})
+
+const getLocationName = (fingerprint: Fingerprint): string => {
+  return 'Hollywood'
+}
+
+xdescribe('getLocationName', () => {
+  describe('when we have not named any yet', () => {
+    it('should return the next name', () => {
+      const actual = getLocationName()
+      const expected = 'Hollywood'
+      expect(actual).toBe(expected)
+    })
+  })
+})
+
 const logEntryToRouteEntry = (logEntry: LogEntry): RouteEntry => {
   const routeEntry: RouteEntry = {
     origin: logEntry.origin,
-    location: logEntry.measurements,
+    location: logEntry.fingerprint,
+    // locationName: getLocationName(logEntry.fingerprint),
   }
   return routeEntry
 }
@@ -30,33 +168,18 @@ const getRoute = (log: Log): Route => {
   return route
 }
 
-const islands: {[key: string]: Island} = {
-  A: [
-    {
-      degrees: 90,
-      distanceEstimate: Estimate.MEDIUM,
-    },
-  ],
-  B: [
-    {
-      degrees: 270,
-      distanceEstimate: Estimate.MEDIUM,
-    },
-  ],
-}
-
 it('should return the route based on the log', () => {
   const log: Log = [
     {
       origin: null,
-      measurements: islands.A,
-      next: islands.A[0],
+      fingerprint: fingerprints.A,
+      next: fingerprints.A[0],
     },
   ]
   const actual = getRoute(log)
   const expected: Route = [
     {
-      location: islands.A,
+      location: fingerprints.A,
       origin: null,
     },
   ]
@@ -67,12 +190,12 @@ test('2 step log', () => {
   const log: Log = [
     {
       origin: null,
-      measurements: islands.A,
-      next: islands.A[0],
+      fingerprint: fingerprints.A,
+      next: fingerprints.A[0],
     },
     {
-      origin: islands.A,
-      measurements: islands.B,
+      origin: fingerprints.A,
+      fingerprint: fingerprints.B,
       next: null,
     },
   ]
@@ -80,11 +203,58 @@ test('2 step log', () => {
   const expected: Route = [
     {
       origin: null,
-      location: islands.A,
+      location: fingerprints.A,
     },
     {
-      origin: islands.A,
-      location: islands.B,
+      origin: fingerprints.A,
+      location: fingerprints.B,
+    },
+  ]
+  expect<Route>(actual).toEqual<Route>(expected)
+})
+
+it('should figure out when it has returned to fingerprint A', () => {
+  // The log doesn't know these fingerprints by name.  We're just adding that for
+  // test convenience.
+  const log: Log = [
+    {
+      origin: null,
+      fingerprint: fingerprints.A,
+      next: fingerprints.A[0],
+    },
+    {
+      origin: fingerprints.A,
+      fingerprint: fingerprints.B,
+      next: fingerprints.B[0],
+    },
+    {
+      origin: fingerprints.B,
+      fingerprint: fingerprints.C,
+      next: fingerprints.C[0],
+    },
+    {
+      origin: fingerprints.C,
+      fingerprint: fingerprints.A,
+      next: null,
+    },
+  ]
+  const actual = getRoute(log)
+  const expected: Route = [
+    {
+      origin: null,
+      location: fingerprints.A,
+    },
+    {
+      origin: fingerprints.A,
+      location: fingerprints.B,
+    },
+    {
+      origin: fingerprints.B,
+      location: fingerprints.C,
+    },
+    {
+      origin: fingerprints.C,
+      location: fingerprints.A,
     },
   ]
   expect<Route>(actual).toEqual<Route>(expected)
